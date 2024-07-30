a11d71993e8fb3d1ca1d3f74b24e91eb323bf3a3
Girish maharana <80371796+BitHeadmr@users.noreply.github.com>
2024-07-30 19:33:54 +0530

    Update firbase.js

--- twiller/src/context/firbase.js
+++ twiller/src/context/firbase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDB8a2d_mk1Qk3YaA8A8bYIrE4VK3T1608",
-  authDomain: "twiller-110d3.firebaseapp.com",
-  projectId: "twiller-110d3",
-  storageBucket: "twiller-110d3.appspot.com",
-  messagingSenderId: "360299699306",
-  appId: "1:360299699306:web:a195ac0bd981029d3a7e13",
-  measurementId: "G-MGERX8360E"
+  apiKey: "ur apikey",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "ur app id ",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
